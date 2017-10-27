af30908e2a023d1ea3909fb79aeab98f0cf3cabb
Christian Ellison <CELLISON2@harding.edu>
2017-10-27 13:08:44 -0500

    Working on Delete_Song, some problems coming up. Made some changes to take out empty rows in DataGrid. Made All Music the default index. Added Play/Stop Button.

--- hTunes/MainWindow.xaml.cs
+++ hTunes/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,7 +45,6 @@ namespace hTunes
         private void LoadGrid(object sender, RoutedEventArgs e)
         {
             var songs = musicLibrary.Songs;
- 
             songGrid.ItemsSource = songs.DefaultView;
         }
 
@@ -62,6 +62,47 @@ namespace hTunes
 
                 songGrid.ItemsSource = songs.DefaultView;
             }
+        }
+
+        private void Delete_Song(object sender, RoutedEventArgs e)
+        {
+            if (playListBox.SelectedItem.ToString() == "All Music")
+            {
+                DataRowView currentItem = songGrid.SelectedItem as DataRowView;
+                int songID = (int)currentItem["id"];
+
+                //https://social.msdn.microsoft.com/Forums/vstudio/en-US/d3f223ac-7fca-486e-8939-adb46e9bf6c9/how-can-i-get-yesno-from-a-messagebox-in-wpf?forum=wpf
+                if (MessageBox.Show("Are you sure you wish to delete this song?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                { }
+                else
+                   if (currentItem != null)
+                    musicLibrary.DeleteSong(songID);
+            }
+            else
+            {
+                DataRowView currentItem = songGrid.SelectedItem as DataRowView;
+
+                ////Crashes when getting songID from a song in a playlist via context menu works for removing songs from All Music
+                ////Not yet sure how to access the song_ID when accessing the listBox via playlist
+                int songID = (int)currentItem["song_id"];
+                int position = (int)currentItem["position"];
+                musicLibrary.RemoveSongFromPlaylist(position, songID, playListBox.SelectedItem.ToString());
+            }
+        }
+
+        private void Play_Song(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void New_Playlist(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Stop_Song(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
